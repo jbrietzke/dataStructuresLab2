6f602ee3ca9bbd92d9177bad3ec08cf1576b7852
Jackson Brietzke <jack.brietzke@gmail.com>
2016-10-07 12:56:08 -0400

    taking couts out

--- Node.cpp
+++ Node.cpp
@@ -4,28 +4,24 @@ using namespace std;
 
 Node::Node()
 {
-   cout << "We are in the default constructor";
    next = NULL;
    studentPtr = new Student();
 }
 
 Node::Node(char x)
 {
-   cout << "We are in the Node character constructor: " << x << endl;
    next = NULL;
    studentPtr = new Student(x);
 }
 
 Node::Node(Student *newStudent)
 {
-   cout << "Hello\n";
    next = NULL;
    studentPtr = newStudent;
 }
 
 Node::~Node()
 {
-   cout << "We are being destroyed\n";
    next = NULL;
 }
 
